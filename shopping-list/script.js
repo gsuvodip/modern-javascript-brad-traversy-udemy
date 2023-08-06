c4bfc5f740d4b6ac313a9a1e26e12a9d97fc0a07
gsuvodip <gsuvodip.webdev@gmail.com>
2023-08-06 21:10:32 +0530

    update and reset state

--- shopping-list/script.js
+++ shopping-list/script.js
@@ -25,7 +25,15 @@ function onAddItemSubmit(e) {
     return;
   }
 
-  //   console.log("success");
+  // check for edit mode
+  if (isEditMode) {
+    const itemToEdit = itemList.querySelector(".edit-mode");
+
+    removeItemFromStorage(itemToEdit.textContent);
+    itemToEdit.classList.remove("edit-mode");
+    itemToEdit.remove();
+    isEditMode = false;
+  }
 
   // add item DOM element
   addItemToDOM(newItem);
@@ -157,6 +165,8 @@ function filterItems(e) {
 }
 
 function checkUI() {
+  itemInput.value = "";
+
   const items = itemList.querySelectorAll("li");
   console.log(items.length);
 
@@ -167,6 +177,11 @@ function checkUI() {
     clearBtn.style.display = "block";
     itemFilter.style.display = "block";
   }
+
+  formBtn.innerHTML = '<i class="fa-solid fa-plus"></i> Add Item';
+  formBtn.style.backgroundColor = "#333";
+
+  isEditMode = false;
 }
 
 // Initialize App
